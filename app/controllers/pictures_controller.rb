4b431fbef4a36f1e5f165dbfbde92098f90fcbab
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-05 18:36:32 -0500

    Finished basic logic

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,46 @@
 class PicturesController < ApplicationController
 	def index
+		@pictures = Picture.all
+	end
+
+	def show
+		@picture = Picture.find(params[:id].to_i)
+	end
+
+	def edit
+		@picture = Picture.find(params[:id].to_i)
+	end
+
+	def update
+		@picture = Picture.find(params[:id].to_i)
+		if @picture.update_attributes(picture_params)
+			redirect_to "/pictures/#{@picture.id}"
+		else
+			render :edit
+		end
+	end
+
+	def delete
+		@picture = Picture.find(params[:id].to_i)
+		@picture.destroy
+		redirect_to pictures_url
+	end
+
+	def new
+		@picture = Picture.new
+	end
+
+	def create
+		@picture = Picture.new(picture_params)
+		if @picture.save
+			redirect_to pictures_url
+		else
+			render :new
+		end
+	end
+
+	private
+	def picture_params
+		params.require(:picture).permit(:artist, :title, :url)
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Picture.create!(
+  :title  => "Pandog",
+  :artist => "Nicholas Haley",
+  :url    => "http://i.huffpost.com/gen/1793202/thumbs/o-PANDA-DOG-570.jpg"
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
